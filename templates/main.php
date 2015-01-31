425e8764cffbf9ba67ca853540ebf6f923854d4b
Dominik Schreiber <domischreib@gmail.com>
2015-01-31 23:00:59 +0100

    added simplest display of all carddata

--- templates/main.php
+++ templates/main.php
@@ -1,2 +1,12 @@
-<h1>It works!</h1>
-<p><?php p($_['msg']); ?></p>
+<style scoped>
+li + li {
+    margin-top: 2em;
+}
+</style>
+
+<h1>Users</h1>
+<ul>
+<?php foreach ($_['users'] as $user) { ?>
+<li><?php p($user) ?></li>
+<?php } ?>
+</ul>
