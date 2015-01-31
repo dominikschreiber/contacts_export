7dbb1606da04fd58f0222cf791f165de151f93ba
Dominik Schreiber <domischreib@gmail.com>
2015-01-31 23:10:22 +0100

    added map reduce to get only names from vcards

--- index.php
+++ index.php
@@ -13,6 +13,19 @@ while ($row = $result->fetchRow()) {
     $users[] = $row['carddata'];
 }
 
+function vcardproperty_to_associativearray($property) {
+    $c = explode('=', $property);
+    return array($c[0] => $c[1]);
+}
+function merge_associativearrays($prev, $now) {
+    return $prev + $now;
+}
+function format_vcard($vcard) {
+    $properties = array_reduce('merge_associativearrays', array_map('vcardproperty_to_associativearray', explode(';', $vcard)), array());
+    return $properties['N'];
+}
+$formatted_users = array_map('format_vcard', $users);
+
 $tpl = new OCP\Template('contacts_export', 'main', 'user');
 $tpl->assign('users', $users);
 $tpl->printPage();
