1a8092abe064a4fd2ddb19b42cfb24b63bc1352e
Dominik Schreiber <domischreib@gmail.com>
2015-01-31 23:00:38 +0100

    added db query to get all carddata

--- index.php
+++ index.php
@@ -3,8 +3,18 @@
 \OCP\User::checkLoggedIn();
 \OCP\App::checkAppEnabled('contacts_export');
 
+$sql = 'SELECT carddata FROM `*prefix*contacts_cards` WHERE addressbookid = 5';
+$args = array();
+$query = \OCP\DB::prepare($sql);
+$result = $query->execute($args);
+
+$users = array();
+while ($row = $result->fetchRow()) {
+    $users[] = $row['carddata'];
+}
+
 $tpl = new OCP\Template('contacts_export', 'main', 'user');
-$tpl->assign('msg', 'Hello World');
+$tpl->assign('users', $users);
 $tpl->printPage();
 
 ?>
