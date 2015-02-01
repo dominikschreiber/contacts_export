5465f1e74448d562b401b17841d79156ece11139
Dominik Schreiber <domischreib@gmail.com>
2015-02-01 15:30:22 +0100

    added structure to procedural code

--- index.php
+++ index.php
@@ -1,33 +1,37 @@
 <?php
-
-\OCP\User::checkLoggedIn();
-\OCP\App::checkAppEnabled('contacts_export');
-
-$sql = 'SELECT carddata FROM `*PREFIX*contacts_cards` WHERE addressbookid = 5';
-$args = array();
-$query = \OCP\DB::prepare($sql);
-$result = $query->execute($args);
-
-$users = array();
-while ($row = $result->fetchRow()) {
-    $users[] = $row['carddata'];
+function init() {
+    \OCP\User::checkLoggedIn();
+    \OCP\App::checkAppEnabled('contacts_export');
 }
 
-function vcardproperty_to_associativearray($property) {
-    $c = explode('=', $property);
-    return array($c[0] => $c[1]);
-}
-function merge_associativearrays($prev, $now) {
-    return $prev + $now;
-}
-function format_vcard($vcard) {
-    $properties = array_reduce('merge_associativearrays', array_map('vcardproperty_to_associativearray', explode(';', $vcard)), array());
-    return $properties['N'];
-}
-$formatted_users = array_map('format_vcard', $users);
+function raw() {
+    $sql = 'SELECT carddata FROM `*PREFIX*contacts_cards` WHERE addressbookid = 5';
+    $args = array();
+    $query = \OCP\DB::prepare($sql);
+    $result = $query->execute($args);
 
-$tpl = new OCP\Template('contacts_export', 'main', 'user');
-$tpl->assign('users', $formatted_users);
-$tpl->printPage();
+    $users = array();
+    while ($row = $result->fetchRow()) {
+        $users[] = $row['carddata'];
+    }
+    
+    return $users;
+}
 
+function format($raw) {
+    return $raw;
+}
+
+function render($formatted) {
+    $tpl = new OCP\Template('contacts_export', 'main', 'user');
+    $tpl->assign('users', $formatted);
+    $tpl->printPage();
+}
+
+function main() {
+    init();
+    render(format(raw()));
+}
+
+main();
 ?>
