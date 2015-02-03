42c3ba89d0671925ff0d10ffc5383f7fde471579
Dominik Schreiber <dschreiber@kellerkinder.de>
2015-02-03 16:33:48 +0100

    added var_dump

--- templates/main.php
+++ templates/main.php
@@ -1,10 +1,8 @@
 <h1>Users</h1>
 <?php foreach ($_["users"] as $user): ?>
-<h2><?php p($user["n"]) ?></h2>
-<dl>
-<?php foreach ($user as $prop => $val): ?>
-    <dt><?php p($prop) ?></dt>
-    <dd><?php p($val) ?></dd>
-<?php endforeach; ?>
-</dl>
+<pre><?php
+ob_start();
+var_dump($user);
+print_unescaped(ob_get_clean());
+?></pre>
 <?php endforeach; ?>
