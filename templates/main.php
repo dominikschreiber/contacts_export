9b4dddc84ba9bc1bcd934a922149ca6faae33626
Dominik Schreiber <dschreiber@kellerkinder.de>
2015-02-03 15:40:04 +0100

    added table output for user

--- templates/main.php
+++ templates/main.php
@@ -1,12 +1,11 @@
-<style scoped>
-li + li {
-    margin-top: 2em;
-}
-</style>
-
 <h1>Users</h1>
-<ul>
 <?php foreach ($_['users'] as $user) { ?>
-<li><?php p($user) ?></li>
+<h2><?php p($user['n']) ?></h2>
+<table>
+    <?php foreach ($user as $prop => $val) { ?>
+    <tr>
+        <th><?php p($prop) ?></th><td><?php p(implode(', ', $val)) ?></td>
+    </tr>
+    <?php } ?>
+</table>
 <?php } ?>
-</ul>
