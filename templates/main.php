0b6089c5943c2156e696f315bf53e6596f8f53f7
Dominik Schreiber <dschreiber@kellerkinder.de>
2015-02-03 15:59:35 +0100

    added filter to remove empty values

--- templates/main.php
+++ templates/main.php
@@ -1,11 +1,10 @@
 <h1>Users</h1>
 <?php foreach ($_['users'] as $user) { ?>
-<h2><?php p($user['n']) ?></h2>
-<table>
+<h2><?php p($user['n']); ?></h2>
+<dl>
     <?php foreach ($user as $prop => $val) { ?>
-    <tr>
-        <th><?php p($prop) ?></th><td><?php p(implode(', ', $val)) ?></td>
-    </tr>
+    <dt><?php p($prop); ?></dt>
+    <dd><?php p(implode(', ', $val)); ?></dd>
     <?php } ?>
-</table>
+</dl>
 <?php } ?>
